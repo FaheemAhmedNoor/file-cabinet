f51f5d99bf549637f10ec7d64de3f7c5abdd6dad
FaheemAhmedNoor <faheemahmednoor12@gmail.com>
2023-04-29 03:26:25 +0500

    Create document CRUD

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,50 @@
+class DocumentsController < ApplicationController
+  def index
+    @docs = Document.all
+  end
+
+  def show
+    @doc = Document.find(params[:id])
+  end
+
+  def new
+    @doc = Document.new
+  end
+
+  def create
+    @doc = Document.new(doc_params)
+    if @doc.save!
+      redirect_to documents_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @doc = Document.find(params[:id])
+  end
+
+  def update
+    @doc = Document.find(params[:id])
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @doc = Document.find(params[:id])
+    if @doc.destroy
+      redirect_to documents_path
+    else
+      render @doc
+    end
+  end
+
+  private
+
+  def doc_params
+    params.require(:document).permit(:id, :title, :content)
+  end
+end
